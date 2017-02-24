6c3f61bbff5177989c643428c3952e96df55fa00
Xyrephon <jtuc1613@740s-pl-115.local>
2017-02-24 07:59:38 -0700

    Added an ArrayList of Account to Bank.

--- src/create/model/Bank.java
+++ src/create/model/Bank.java
@@ -1,6 +1,16 @@
 package create.model;
 
+import java.util.ArrayList;
+
 public class Bank 
 {
+	private ArrayList<Account> accounts;
+	
+	public Bank()
+	{
+		accounts = new ArrayList<Account>();
+	}
+	
+	
 
 }
